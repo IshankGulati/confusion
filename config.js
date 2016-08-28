cfe19f4e08d3e45a326adf9b358bc272efb5e426
IshankGulati <gulati.ishank@gmail.com>
2016-08-28 18:53:37 +0530

    first working deploy on heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : 'mongodb://admin:qwerty0987@ds017896.mlab.com:17896/confusion',
     facebook : {
         'clientID' : '171144859984708',
         'clientSecret' : 'a97fefd20b2f929b0024e8c2a6d901e8',
